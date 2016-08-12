2d18fa12e16f7009fa2b39ef00ff7438ad3863f7
Tomas Vondra <tv@fuzzy.cz>
2016-08-12 22:54:00 +0200

    fix trimmed.c header (filename, copyright years)

--- src/trimmed.c
+++ src/trimmed.c
@@ -1,6 +1,6 @@
 /*
-* quantile.c - Trimmed aggregate functions
-* Copyright (C) Tomas Vondra, 2011
+* trimmed.c - Trimmed aggregate functions
+* Copyright (C) Tomas Vondra, 2011-2016
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
